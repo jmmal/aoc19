6e32382de136ebc08adae900dec4077c12c45c24
Josh Maloney <jmmal@users.noreply.github.com>
2019-12-09 22:52:18 +1100

    day 9

--- day9/day9.go
+++ day9/day9.go
@@ -5,20 +5,23 @@ import (
 	"advent-of-code-2019/intcode"
 )
 
-func main() {
-	var program = intcode.ReadProgram()
-	
+func boost(mem []int64) {
 	input := make(chan int64, 1)
 	output := make(chan int64, 20)
 	halt := make(chan bool)
 
-	computer := intcode.Computer{Memory: program, IP: 0, RB: 0}
+	computer := intcode.Computer{Memory: mem, IP: 0, RB :0}
 
 	go computer.Execute(input, output, halt)
 
 	input <- 1
-
+	// input <- 2
 	for val := range output {
 		fmt.Println(val)
 	}
+}
+
+func main() {
+	program := intcode.ReadProgram()
+	boost(program)
 }
--- intcode/intcode.go
+++ intcode/intcode.go
@@ -2,82 +2,125 @@ package intcode
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
+const (
+	Add         = 1
+	Mul         = 2
+	Input       = 3
+	Output      = 4
+	JumpIfTrue  = 5
+	JumpIfFalse = 6
+	LessThan    = 7
+	Equals      = 8
+	Rel         = 9
+	Halt        = 99
+)
+
+const (
+	Position  = 0
+	Immediate = 1
+	Relative  = 2
+)
+
+// Computer represents structure to run intcode
+type Computer struct {
+	Memory []int64
+	IP     int64
+	RB     int64
+}
+
 // Execute the opcode
-func Execute(mem []int, input <-chan int, output chan<- int, halt chan<- bool) {
-	arr := make([]int, len(mem))
-	copy(arr, mem)
+func (computer Computer) Execute(input <-chan int64, output chan<- int64, halt chan<- bool) {
+	arr := make([]int64, 10000)
+	copy(arr, computer.Memory)
 
-	ip := 0
-	
+	computer.Memory = arr
+	computer.IP = 0
+	computer.RB = 0
+
 	for {
-		op, modes := getOpAndModes(arr[ip])
-		
+		op, modes := computer.getOpAndModes()
+
 		switch op {
-		case 1:
-			a := getArg(ip+1, arr, modes[0])
-			b := getArg(ip+2, arr, modes[1])
-			c := arr[ip+3]
-			
-			arr[c] = a + b
-			ip += 4
-		case 2:
-			a := getArg(ip+1, arr, modes[0])
-			b := getArg(ip+2, arr, modes[1])
-			c := arr[ip+3]
-			arr[c] = a * b
-			ip += 4
-		case 3:
-			arr[arr[ip+1]] = <-input
-			ip += 2
-		case 4:
-			a := getArg(ip+1, arr, modes[0])
-			output<-a
-			ip += 2
-		case 5:
-			a := getArg(ip+1, arr, modes[0])
-			b := getArg(ip+2, arr, modes[1])
-			
-			if a != 0 {
-				ip = b
-				} else {
-				ip += 3
-			}
-		case 6:
-			a := getArg(ip+1, arr, modes[0])
-			b := getArg(ip+2, arr, modes[1])
+		case Add:
+			a := computer.getArg(1, modes[0])
+			b := computer.getArg(2, modes[1])
+			c := computer.getArg(3, modes[2])
 
-			if a == 0 {
-				ip = b
+			computer.Memory[c] = computer.Memory[a] + computer.Memory[b]
+			computer.IP += 4
+
+		case Mul:
+			a := computer.getArg(1, modes[0])
+			b := computer.getArg(2, modes[1])
+			c := computer.getArg(3, modes[2])
+			computer.Memory[c] = computer.Memory[a] * computer.Memory[b]
+			computer.IP += 4
+
+		case Input:
+			a := computer.getArg(1, modes[0])
+			computer.Memory[a] = <-input
+			computer.IP += 2
+
+		case Output:
+			a := computer.getArg(1, modes[0])
+			output <- computer.Memory[a]
+			computer.IP += 2
+
+		case JumpIfTrue:
+			a := computer.getArg(1, modes[0])
+			b := computer.getArg(2, modes[1])
+
+			if computer.Memory[a] != 0 {
+				computer.IP = computer.Memory[b]
 			} else {
-				ip += 3
+				computer.IP += 3
 			}
-		case 7:
-			a := getArg(ip+1, arr, modes[0])
-			b := getArg(ip+2, arr, modes[1])
-			c := arr[ip+3]
+		case JumpIfFalse:
+			a := computer.getArg(1, modes[0])
+			b := computer.getArg(2, modes[1])
 
-			if a < b {
-				arr[c] = 1
+			if computer.Memory[a] == 0 {
+				computer.IP = computer.Memory[b]
 			} else {
-				arr[c] = 0
+				computer.IP += 3
 			}
-			ip += 4
-		case 8:
-			a := getArg(ip+1, arr, modes[0])
-			b := getArg(ip+2, arr, modes[1])
-			c := arr[ip+3]
+		case LessThan:
+			a := computer.getArg(1, modes[0])
+			b := computer.getArg(2, modes[1])
+			c := computer.getArg(3, modes[2])
 
-			if a == b {
-				arr[c] = 1
+			if computer.Memory[a] < computer.Memory[b] {
+				computer.Memory[c] = 1
 			} else {
-				arr[c] = 0
+				computer.Memory[c] = 0
+			}
+			computer.IP += 4
+
+		case Equals:
+			a := computer.getArg(1, modes[0])
+			b := computer.getArg(2, modes[1])
+			c := computer.getArg(3, modes[2])
+
+			if computer.Memory[a] == computer.Memory[b] {
+				computer.Memory[c] = 1
+			} else {
+				computer.Memory[c] = 0
 			}
 
-			ip += 4
-		case 99:
-			halt <- true 
+			computer.IP += 4
+
+		case Rel:
+			a := computer.getArg(1, modes[0])
+			computer.RB += computer.Memory[a]
+			computer.IP += 2
+
+		case Halt:
+			close(output)
 			return
 		default:
 			panic(fmt.Sprintf("Unknown instruction %d\n", op))
@@ -85,21 +128,49 @@ func Execute(mem []int, input <-chan int, output chan<- int, halt chan<- bool) {
 	}
 }
 
-func getOpAndModes(instruction int) (int, []int) {
+func (computer Computer) getOpAndModes() (int64, []int64) {
+	instruction := computer.Memory[computer.IP]
+
 	op := instruction % 100
 	mode1 := (instruction / 100) % 10
 	mode2 := (instruction / 1000) % 10
 	mode3 := (instruction / 10000) % 10
 
-	return op, []int{mode1, mode2, mode3}
+	return op, []int64{mode1, mode2, mode3}
 }
 
-func getArg(index int, memory []int, mode int) int {
-	if mode == 0 {
-		return memory[memory[index]]
-	} else if mode == 1 {
-		return memory[index]
+func (computer Computer) getArg(index int64, mode int64) int64 {
+	switch mode {
+	case Position:
+		return computer.Memory[index+computer.IP]
+	case Immediate:
+		return index + computer.IP
+	case Relative:
+		return computer.Memory[index+computer.IP] + computer.RB
+
 	}
 
 	panic("Found unexpected mode")
+}
+
+// ReadProgram reads an int64Code program from file 'input.txt'
+func ReadProgram() []int64 {
+	file, err := ioutil.ReadFile("input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	strarr := strings.Split(string(file), ",")
+	var ints = []int64{}
+	for _, stringVal := range strarr {
+		val, err := strconv.ParseInt(stringVal, 10, 0)
+
+		if err != nil {
+			panic(err)
+		}
+
+		ints = append(ints, val)
+	}
+	return ints
 }
